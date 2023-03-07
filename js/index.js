6de457a58a35d312d8c5929b027d9de9d8228d8d
Bhuwan597 <v1acharya34@gmail.com>
2023-03-08 03:47:26 +0545

    commit

--- js/index.js
+++ js/index.js
@@ -6,3 +6,21 @@ menuToggle.addEventListener('click', ()=>{
     right.classList.toggle('active')
     navbarTop.classList.toggle('active')
 })
+
+var typed = new Typed('#multi-text',
+{
+    strings:[
+        "Test Books",
+        'Refrence Books',
+        'Guide and Guess Papers',
+        'Handwritten Notes',
+        'Courses',
+        'Religious Books',
+        'Motivational Books',
+        'Other Books',
+    ],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
